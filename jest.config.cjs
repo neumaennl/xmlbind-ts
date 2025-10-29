d938fb4f8571dc9e25e1f0c32ca9113caa6cd88c
Neumaennl <neumaennl@gmx.net>
2025-10-29 04:34:50 +0100

    moved jest-junit config to jest config and improved reporting

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,4 +2,16 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   testMatch: ["**/__tests__/**/*.test.ts"],
+  reporters: [
+    "default",
+    [
+      "jest-junit",
+      {
+        outputDirectory: "test-results",
+        outputName: "jest-junit.xml",
+        classNameTemplate: "{filename}",
+        titleTemplate: "{title}",
+      },
+    ],
+  ],
 };
